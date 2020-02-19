cfad03d3c78a2868f2f7e89b7a0a081566f74353
Talha Abbasi <talhaabbasi1997@gmail.com>
2020-02-19 19:49:02 +0500

    Initial Commit

---
+++ src/ProductList.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import { Table } from "react-bootstrap";
+
+class ProductList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>
+        <h2>Product List</h2>
+      </div>
+    );
+  }
+}
+
+export default ProductList;
